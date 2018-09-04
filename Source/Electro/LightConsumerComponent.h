22ebf0b4ea2b1dfa8f8895fa18800f8bb1d5ff5d
jbriquet <jules.briquet@gmail.com>
2018-09-04 23:53:37 +0200

    Basic Level First Setup + First Classes Scretch

---
+++ Source/Electro/LightConsumerComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ElectricityConsumerComponent.h"
+#include "LightConsumerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Electricity), meta = (BlueprintSpawnableComponent))
+class ELECTRO_API ULightConsumerComponent : public UElectricityConsumerComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
